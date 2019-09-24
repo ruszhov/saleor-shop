a2651e52c1f67982db75fc4a4032a33159b18da9
ruszhov <ruszhov@ukr.net>
2019-09-24 17:30:20 +0300

    form/pagination updating

--- saleor/static/dashboard/js/components/akvarium.js
+++ saleor/static/dashboard/js/components/akvarium.js
@@ -1,14 +1,43 @@
+var createCookie = function(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+
 //Returning to pginated page list
 let rows = document.getElementsByClassName('avatar');
 for (let item of rows) {
   item.onclick = function () {
-    sessionStorage.setItem("current_url", window.location.href);
+    createCookie('paged_url', window.location.href);
   };
-}
+};
 
 let goToPaginated = document.getElementById('paginated_url');
 if (goToPaginated != null) {
   goToPaginated.onclick = function () {
-    window.location.replace(sessionStorage.getItem("current_url"));
+    let paged_url = getCookie('paged_url');
+    window.location.replace(paged_url);
   };
 };
