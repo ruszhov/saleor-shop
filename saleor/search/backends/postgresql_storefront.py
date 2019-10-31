45f18fc0f24d893e04ac4aced4ad1ee55df09c2b
ruszhov <ruszhov@ukr.net>
2019-10-31 16:03:35 +0200

    search on frontend updating

--- saleor/search/backends/postgresql_storefront.py
+++ saleor/search/backends/postgresql_storefront.py
@@ -2,11 +2,13 @@ from django.contrib.postgres.search import (
     SearchQuery, SearchRank, SearchVector)
 
 from django.contrib.postgres.search import TrigramSimilarity
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from ...product.models import Product
 from django.utils import translation
 import re
+from django.contrib.postgres.search import SearchVector, SearchQuery
+from django.utils import translation
 
 def has_cyrillic(text):
     return bool(re.search('[а-яА-Я]', text))
@@ -23,17 +25,25 @@ def search(phrase):
 
     """
     name_sim = TrigramSimilarity('name', phrase)
-    tr_name = TrigramSimilarity('translations__name', phrase)
     sku = TrigramSimilarity('variants__sku', phrase)
     published = Q(is_published=True)
     ft_in_description = Q(description__search=phrase)
     name_similar = Q(name_sim__gt=0.2)
-    tr_name_similar = Q(tr_name__gt=0.2)
     sku_similar = Q(sku__gt=0.7)
+    lang = translation.get_language()
+
+    vector = SearchVector('translations__name', weight='A') + \
+             SearchVector('translations__language_code', weight='C') + \
+             SearchVector('translations__description', wheight='B')
+
     if phrase.isalpha():
         if has_cyrillic(phrase) is True:
-            return Product.objects.annotate(tr_name=tr_name).filter((ft_in_description | tr_name_similar) & published)
+            return Product.objects.annotate(search=vector).filter(search=SearchQuery(lang) & SearchQuery(phrase))
         else:
-            return Product.objects.annotate(name_sim=name_sim).filter((ft_in_description | name_similar) & published)
+            en_res = Product.objects.annotate(name_sim=name_sim).filter((ft_in_description | name_similar) & published)
+            if en_res.count() != 0:
+                return en_res
+            else:
+                return Product.objects.annotate(search=vector).filter(search=SearchQuery(phrase) & SearchQuery('pl'))
     else:
         return Product.objects.annotate(sku=sku).filter(sku_similar & published)
